28506f30162667e973a1be57c77d240ea0242a75
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2020-10-31 01:49:04 -0500

    more complex test js

--- functions/test.js
+++ functions/test.js
@@ -1,6 +1,22 @@
 exports.handler = async function(event, context) {
-    return {
-        statusCode: 200,
-        body: JSON.stringify({message: "Hello World"})
-    };
+    event.replaceResponse(async () => {
+        const originResponse = await fetch(event.request);
+        const transformation = new TransformStream({
+          flush(controller) {
+            const encoder = new TextEncoder();
+            const buf = encoder.encode(
+              "<p>Served from a Netlify Edge Handler</p>");
+            controller.enqueue(buf);
+          },
+        });
+
+        const transformedBody = originResponse.body.pipeThrough(transformation);
+        const headers = {
+          'Content-Type': 'text/html'
+        };
+
+        return new Response(transformedBody, {
+          headers
+        });
+  });
 }
