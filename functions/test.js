6d891acff747080ef07fbaeb1b5d3999a3747e88
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2020-11-01 09:30:28 -0600

    echo request

--- functions/test.js
+++ functions/test.js
@@ -1,22 +1,9 @@
 exports.handler = async function(event, context) {
-    event.replaceResponse(async () => {
-        const originResponse = await fetch(event.request);
-        const transformation = new TransformStream({
-          flush(controller) {
-            const encoder = new TextEncoder();
-            const buf = encoder.encode(
-              "<p>Served from a Netlify Edge Handler</p>");
-            controller.enqueue(buf);
-          },
-        });
-
-        const transformedBody = originResponse.body.pipeThrough(transformation);
-        const headers = {
-          'Content-Type': 'text/html'
-        };
-
-        return new Response(transformedBody, {
-          headers
-        });
-  });
+    console.log(event)
+    console.log(event.path)
+    console.log(event.httpMethod)
+    console.log(event.headers)
+    console.log(event.queryStringParameters)
+    console.log(event.body)
+    console.log(event.isBase64Encoded)
 }
