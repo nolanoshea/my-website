ed51a2110848a982ce54e50caa9b9bb8aac5755f
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2020-11-01 10:40:17 -0600

    node js style get request

--- functions/test.js
+++ functions/test.js
@@ -1,4 +1,24 @@
 exports.handler = async function(event, context) {
-    fetch('https://registry.jsonresume.org/nolanoshea')
-        .then(res => console.log(res))
+    const https = require('https')
+    const options = {
+      hostname: 'registry.jsonresume.org',
+      port: 443,
+      path: '/nolanoshea',
+      method: 'GET'
+    }
+    
+    let output = ''
+
+    const req = https.request(options, res => {
+        res.on('data', chunk => output += chunk)
+            .on('end', () => {
+                let obj = JSON.parse(output)
+
+                console.log(obj)
+                onResult(res.statusCode, obj)
+            })
+    })
+
+    req.on('error', error => console.error(error.message))
+    req.end()
 }
