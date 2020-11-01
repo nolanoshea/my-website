478bd1f89ac64411c95f8b6bb523f5990dfd1983
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2020-11-01 10:14:30 -0600

    try serverless proxying

--- functions/test.js
+++ functions/test.js
@@ -1,9 +1,4 @@
 exports.handler = async function(event, context) {
-    console.log(event)
-    console.log(event.path)
-    console.log(event.httpMethod)
-    console.log(event.headers)
-    console.log(event.queryStringParameters)
-    console.log(event.body)
-    console.log(event.isBase64Encoded)
+    fetch('https://registry.jsonresume.org/nolanoshea')
+        .then(res => console.log(res))
 }
