0f3a9c40fc08cd320072b13bd3c18eb7920aa69d
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2020-11-01 11:21:26 -0600

    code cleanup

--- functions/test.js
+++ functions/test.js
@@ -6,26 +6,21 @@ exports.handler = async (event) => {
     
     const response = await new Promise((resolve, reject) => {
         const req = https.get(url, res => {
-          res.on('data', chunk => {
-            dataString += chunk
-          });
-          res.on('end', () => {
-//               let obj = JSON.parse(dataString)
-              console.log(dataString)
-//             resolve({
-//                 statusCode: 200,
-//                 body: JSON.stringify(JSON.parse(dataString), null, 4)
-//             })
-          })
+          res.on('data', chunk => dataString += chunk)
+            .on('end', () => {
+                resolve({
+                    statusCode: 200,
+                    body: dataString
+                })
+            })
         })
         
         req.on('error', (e) => {
           reject({
               statusCode: 500,
-              body: 'Something went wrong!'
+              body: e.message
           })
         })
     })
-    
     return response
 }
