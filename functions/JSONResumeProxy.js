ddf30b4ca52051d4359b169cad4a4a4c9f98078f
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2021-04-28 01:11:19 -0500

    remove project xp and re-add Sabre

--- functions/JSONResumeProxy.js
+++ functions/JSONResumeProxy.js
@@ -10,7 +10,17 @@ const removeLinkDetail = '<script>' +
       'document.getElementsByClassName("icon-link")[0].parentNode.parentNode.remove()' +
       '</script>'
 
-const snippets = darkModeAPI + removeLinkDetail
+const addSabre = '<script>' +
+      '[...document.getElementsByClassName("card-nested clearfix")].forEach(e => e.querySelector(".clear-margin").lastChild.textContent += "Sabre")' +
+      '</script>'
+
+const removeProjects = '<script>' +
+      'document.getElementById("projects-experience").remove()' +
+      // update floating nav
+      'document.querySelectorAll("a[href=\"#projects-experience\"]")[0].parentNode.remove()' +
+      '</script>'
+
+const snippets = removeProjects + darkModeAPI + addSabre + removeLinkDetail
 
 exports.handler = async (event) => {
     let dataString = ''
