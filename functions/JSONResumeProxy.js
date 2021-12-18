d0c29137b040124db163ef0705918701c5b84ab7
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2021-12-18 16:30:00 -0600

    debug js error

--- functions/JSONResumeProxy.js
+++ functions/JSONResumeProxy.js
@@ -3,7 +3,7 @@ const url = 'https://registry.jsonresume.org/nolanoshea'
 
 const darkModeAPI = '<script async src="https://cdn.jsdelivr.net/npm/darkreader/darkreader.min.js" onload="' +
       'DarkReader.setFetchMethod(window.fetch);' +
-      'DarkReader.auto();' +
+      'DarkReader.auto(true);' +
       '"></script>'
 
 const removeLinkDetail = '<script>' +
