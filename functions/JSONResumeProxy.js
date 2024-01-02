02ee94f96fafb675a8f81a35fdcc25728a3b25db
nolanoshea <65926793+nolanoshea@users.noreply.github.com>
2024-01-02 17:40:30 -0600

    remove custom js after schema fixes

--- functions/JSONResumeProxy.js
+++ functions/JSONResumeProxy.js
@@ -10,16 +10,7 @@ const removeLinkDetail = '<script>' +
       'document.getElementsByClassName("icon-link")[0].parentNode.parentNode.remove()' +
       '</script>'
 
-const addSabre = '<script>' +
-      '[...document.getElementsByClassName("card-nested clearfix")].forEach(e => e.querySelector(".clear-margin").lastChild.textContent += "Sabre")' +
-      '</script>'
-
-const removeProjects = '<script>' +
-      'document.getElementById("projects-experience").remove();' +
-      // update floating nav
-      'document.querySelectorAll("a[href=\\"#projects-experience\\"]")[0].parentNode.remove();' +
-      '</script>'
-
+// add back once renewed
 const fixBadge = '<script>' +
       'let badgeNode = document.querySelector(\'[itemprop="certificate"]\').childNodes[0];' +
       'badgeNode.innerHTML = badgeNode.innerHTML.replace(/&lt;/, "<").replace(/&gt;/, ">")' +
@@ -27,7 +18,7 @@ const fixBadge = '<script>' +
       '.replace(/,/, "")' +
       '</script>'
 
-const snippets = removeProjects + darkModeAPI + fixBadge + addSabre + removeLinkDetail
+const snippets = darkModeAPI + removeLinkDetail
 
 exports.handler = async (event) => {
     let dataString = ''
